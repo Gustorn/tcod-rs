54a08fb080690d0bcb1c3fa8cabf2621e3a5a17b
Gustorn <gustorn@gmail.com>
2015-04-12 01:25:59 +0200

    Removed some exposed native types
    Since this is a safe wrapper it is better not to provide methods that could potentially cause unsafe behavior, especially when traits can solve this problem wonderfully: the unsafe methods were removed from Color's API. They were moved into the implementations of the AsNative and FromNative traits, both of which the user will explicitly have to include if they want to use the native types. As an additional safety net, there's also an unsafe qualifier on the traits' respective methods.

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,7 @@
 extern crate std;
 
 use bindings::ffi;
-use bindings::{c_bool, c_uint, CString, keycode_from_u32};
+use bindings::{AsNative, FromNative, c_bool, c_uint, CString, keycode_from_u32};
 
 use colors::Color;
 use input::{Key, KeyPressFlags, KeyState};
@@ -77,14 +77,14 @@ impl Root {
 
     pub fn get_fading_color(&self) -> Color {
         unsafe {
-            Color::from_tcod_color_t(
+            FromNative::from_native(
                 ffi::TCOD_console_get_fading_color())
         }
     }
 
     pub fn set_fade(&mut self, fade: u8, fading_color: Color) {
         unsafe {
-            ffi::TCOD_console_set_fade(fade, fading_color.to_color_t());
+            ffi::TCOD_console_set_fade(fade, fading_color.as_native());
         }
     }
 
@@ -188,7 +188,7 @@ pub trait Console {
      
     fn set_key_color(&mut self, color: Color) {
         unsafe {
-            ffi::TCOD_console_set_key_color(self.con(), color.to_color_t());
+            ffi::TCOD_console_set_key_color(self.con(), color.as_native());
         }
     }
 
@@ -206,32 +206,32 @@ pub trait Console {
 
     fn set_default_background(&mut self, color: Color) {
         unsafe {
-            ffi::TCOD_console_set_default_background(self.con(), color.to_color_t());
+            ffi::TCOD_console_set_default_background(self.con(), color.as_native());
         }
     }
 
     fn set_default_foreground(&mut self, color: Color) {
         unsafe {
-            ffi::TCOD_console_set_default_foreground(self.con(), color.to_color_t());
+            ffi::TCOD_console_set_default_foreground(self.con(), color.as_native());
         }
     }
 
     fn console_set_key_color(&mut self, color: Color) {
         unsafe {
-            ffi::TCOD_console_set_key_color(self.con(), color.to_color_t());
+            ffi::TCOD_console_set_key_color(self.con(), color.as_native());
         }
     }
 
     fn get_char_background(&self, x: i32, y: i32) -> Color {
         unsafe {
-            Color::from_tcod_color_t(
+            FromNative::from_native(
                 ffi::TCOD_console_get_char_background(self.con(), x, y))
         }
     }
 
     fn get_char_foreground(&self, x: i32, y: i32) -> Color {
         unsafe {
-            Color::from_tcod_color_t(
+            FromNative::from_native(
                 ffi::TCOD_console_get_char_foreground(self.con(), x, y))
         }
     }
@@ -288,7 +288,7 @@ pub trait Console {
         unsafe {
             ffi::TCOD_console_set_char_background(self.con(),
                                                   x, y,
-                                                  color.to_color_t(),
+                                                  color.as_native(),
                                                   background_flag as u32)
         }
     }
@@ -298,7 +298,7 @@ pub trait Console {
         unsafe {
             ffi::TCOD_console_set_char_foreground(self.con(),
                                                   x, y,
-                                                  color.to_color_t());
+                                                  color.as_native());
         }
     }
 
@@ -320,8 +320,8 @@ pub trait Console {
         unsafe {
             ffi::TCOD_console_put_char_ex(self.con(),
                                           x, y, glyph as i32,
-                                          foreground.to_color_t(),
-                                          background.to_color_t());
+                                          foreground.as_native(),
+                                          background.as_native());
         }
     }
 
